585b69172a8595f6692c9d13a50a4527ce65a807
bkoz <bkozdemba@gmail.com>
2025-01-29 21:35:59 +0000

    updated

--- 01-download-data.py
+++ 01-download-data.py
@@ -12,13 +12,23 @@ import logging
 import requests
 import json
 
-def respond(collection, query):
+def semantic_search(collection:weaviate.collections.Collection, query: str)-> dict:
 
     response = collection.query.near_text(
         query=query,
         limit=1
     )
     return response.objects[0].properties
+
+
+def generative_search(collection:weaviate.collections.Collection, query: str,
+                      task='Summarize', limit=1)-> dict:
+    response = collection.generate.near_text(
+        query=query,
+        limit=1,
+        grouped_task=task
+    )
+    return response.generated
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -47,9 +57,11 @@ dataset.upload_dataset(client)  # Pass the Weaviate client instance
 collections = client.collections.list_all()
 
 collection = client.collections.get('JeopardyQuestion')
-logging.info(respond(collection=collection, query='guitar'))
+logging.info(semantic_search(collection=collection, query='guitar'))
+logging.info(generative_search(collection=collection, query='guitar',
+                      task='Summarize', limit=1))
 collection = client.collections.get('JeopardyCategory')
-logging.info(respond(collection=collection, query='guitar'))
+logging.info(semantic_search(collection=collection, query='guitar'))
 
 # logging.info('*** GET ***')
 # logging.info(client.collections.get)
